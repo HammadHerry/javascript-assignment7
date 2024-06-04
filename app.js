88c01b1e5b4ef7a93e598bb7c4dd95bfef888ea2
HammadHerry <hammadherry81@gmail.com>
2024-06-04 06:52:14 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+ var a = 2 + (2 * 6);
+ a = 14;
+ console.log(a);
+
+  var b = (2 + 2) * 6;
+  b = 24;
+  console.log(b);
+
+  var c = (2 + 2) * (4 + 2);
+  c = 24;
+  console.log(c);
+
+
+  var d = ((2 + 2) * 4) + 2;
+  d = 18;
+  console.log(d);
+
+ 
+
+var e =  2 + 2 * 4 + 10;
+e = 56;
+console.log(e);
+
+
+
+var f = 10 + 2 + 2 * 4;
+console.log(f)
+
+
+var g = 4 / 2 * 4; 
+console.log(g);
